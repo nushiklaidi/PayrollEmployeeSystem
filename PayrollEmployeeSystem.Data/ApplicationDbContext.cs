7c196d6298562f613eec9e4dedf45dc5bd8d8ccd
Klajdi Nushi <cloodee@hotmail.com>
2020-01-25 16:39:10 +0200

    Initial DbContext

---
+++ PayrollEmployeeSystem.Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PayrollEmployeeSystem.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayrollEmployeeSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PaymentRecord> PaymentRecords { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<TaxYear> TaxYears { get; set; }
+    }
+}
