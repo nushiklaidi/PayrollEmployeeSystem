b493a974332e0087d8c32f2aad6d26c1ccb3ada0
Klajdi Nushi <cloodee@hotmail.com>
2020-01-25 15:26:20 +0200

    Code First

--- Startup.cs
+++ Startup.cs
@@ -20,6 +20,7 @@ namespace PayrollEmployeeSystem
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
         }
 
         public IConfiguration Configuration { get; }
