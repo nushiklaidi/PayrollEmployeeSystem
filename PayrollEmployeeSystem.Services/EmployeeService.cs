7c48c9ad5f2fb0565b0c487aefd176035cf3ccb0
Klajdi Nushi <cloodee@hotmail.com>
2020-01-25 18:05:39 +0200

    Create EmployeeService

---
+++ PayrollEmployeeSystem.Services/EmployeeService.cs
@@ -0,0 +1,60 @@
+﻿using PayrollEmployeeSystem.Data;
+using PayrollEmployeeSystem.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollEmployeeSystem.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private ApplicationDbContext _context;
+
+        public EmployeeService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(Employee employee)
+        {
+            await _context.Employees.AddAsync(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task Delete(int employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetById(int employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal StudentLoanRepaymentAmount(int id, decimal totalAmount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal UnionFees(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(int employeeId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PayrollEmployeeSystem.Services/IEmployeeService.cs
@@ -0,0 +1,20 @@
+﻿using PayrollEmployeeSystem.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollEmployeeSystem.Services
+{
+    public interface IEmployeeService
+    {
+        Task CreateAsync(Employee employee);
+        Employee GetById(int employeeId);
+        Task UpdateAsync(Employee employee);
+        Task UpdateAsync(int employeeId);
+        Task Delete(int employeeId);
+        decimal UnionFees(int id);
+        decimal StudentLoanRepaymentAmount(int id, decimal totalAmount);
+        IEnumerable<Employee> GetAll();
+    }
+}
