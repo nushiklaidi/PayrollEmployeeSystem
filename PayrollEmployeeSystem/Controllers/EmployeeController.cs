ca45a979d8638c71d25e46eba60595d178469976
Klajdi Nushi <cloodee@hotmail.com>
2020-01-26 10:46:57 +0200

    Create  EmployeeCreateVM

--- PayrollEmployeeSystem/Controllers/EmployeeController.cs
+++ PayrollEmployeeSystem/Controllers/EmployeeController.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PayrollEmployeeSystem.Models;
+using PayrollEmployeeSystem.Services;
 
 namespace PayrollEmployeeSystem.Controllers
 {
     public class EmployeeController : Controller
     {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
         public IActionResult Index()
         {
-            return View();
+            var employees = _employeeService.GetAll()
+                .Select(employee => new EmployeeIndexViewModel
+                { 
+                    Id = employee.Id,
+                    EmployeeNo = employee.EmployeeNo,
+                    ImageUrl =  employee.ImageUrl,
+                    FullName = employee.FullName,
+                    Gender = employee.Gender,
+                    Designation = employee.Designation,
+                    City = employee.City,
+                    DateJoined = employee.DateJoined
+                }).ToList();
+
+            return View(employees);
         }
     }
 }
