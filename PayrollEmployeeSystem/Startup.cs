79d9ba37971cc9745d805d5aeef61cf51555a6c1
Klajdi Nushi <cloodee@hotmail.com>
2020-02-07 17:14:37 +0200

    Fix Library Class

--- PayrollEmployeeSystem/Startup.cs
+++ PayrollEmployeeSystem/Startup.cs
@@ -12,6 +12,7 @@ using PayrollEmployeeSystem.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PayrollEmployeeSystem.Services;
 
 namespace PayrollEmployeeSystem
 {
@@ -35,6 +36,9 @@ namespace PayrollEmployeeSystem
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            //DI
+            services.AddScoped<IEmployeeService, EmployeeService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
