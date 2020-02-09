144c6c0b84288c0a2847531c4664508806b82e90
Klajdi Nushi <cloodee@hotmail.com>
2020-02-09 13:04:19 +0200

    Create ITaxService

---
+++ PayrollEmployeeSystem.Services/ITaxService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayrollEmployeeSystem.Services
+{
+    public interface ITaxService
+    {
+        decimal TaxAmount(decimal totalAmount);
+    }
+}
