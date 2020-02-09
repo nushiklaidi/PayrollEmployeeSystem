4fd0fe8cd8814293e52f57f6219f0bce2fe2e2d1
Klajdi Nushi <cloodee@hotmail.com>
2020-02-09 13:26:10 +0200

    Create NationalInsuranceContributionService Services

---
+++ PayrollEmployeeSystem.Services/INationalInsuranceContributionService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayrollEmployeeSystem.Services
+{
+    public interface INationalInsuranceContributionService
+    {
+        decimal NIContribution(decimal tatalAmount);
+    }
+}
